00c03b047c70b60a91ae2f937e38e7412b3c0b87
Tarik <tarik.badawy@studium.uni-hamburg.de>
2024-01-17 20:22:51 +0100

    feat: store parsed news page in json

--- parse-news.py
+++ parse-news.py
@@ -2,43 +2,46 @@
 
 import argparse
 import re
-import csv
+import sys
 import bs4
+import json
 
 parser = argparse.ArgumentParser(prog='HackerNewsFrontpageParser')
 parser.add_argument('infile', type=argparse.FileType('r'))
 parser.add_argument('outfile', type=argparse.FileType('w'))
+parser.add_argument('--run', action='store_true') # safety switch
 args = parser.parse_args()
 
 html = bs4.BeautifulSoup(args.infile, 'html.parser')
-writer = csv.writer(args.outfile)
 
-writer.writerow(['Rank', 'Title', 'Site', 'Points', 'Author', 'Timestamp', 'Age', 'Comments'])
+records = []
+
 for news_item in html.find_all('tr', class_='athing'):
-    rank = news_item.find('span', class_='rank').text.strip('.')
-    title = news_item.find('span', class_='titleline').a.text
-    site = news_item.find('span', class_='titleline').a['href']
+    record = {}
+    record['id'] = news_item['id']
+    record['rank'] = news_item.find('span', class_='rank').text.strip('.')
+    record['title'] = news_item.find('span', class_='titleline').a.text
+    record['site'] = news_item.find('span', class_='titleline').a['href']
 
     points = news_item.next_sibling.find('span', class_='score')
     if points != None:
-        points = points.text.split()[0]
-    else:
-        points = ""
+        record['points'] = points.text.split()[0]
 
     author = news_item.next_sibling.find('a', class_='hnuser')
     if author != None:
-        author = author.text
-    else:
-        author = ""
+        record['author'] = author.text
 
-    timestamp = news_item.next_sibling.find('span', class_='age')['title']
-    age = news_item.next_sibling.find('span', class_='age').a.text
+    record['timestamp'] = news_item.next_sibling.find('span', class_='age')['title']
+    record['age'] = news_item.next_sibling.find('span', class_='age').a.text
 
     comments = news_item.next_sibling.find('a', string=re.compile('comments'))
     if comments != None:
-        comments = comments.text.split()[0]
-    else:
-        comments = ""
+        record['comments'] = comments.text.split()[0]
 
-    writer.writerow([rank, title, site, points, author, timestamp, age, comments])
+    records.append(record)
 
+filename = args.outfile.name
+if args.run:
+    json.dump(records, args.outfile, indent=2)
+else:
+    print(f'Would write to {filename}. Use --run to actually write.', file=sys.stderr)

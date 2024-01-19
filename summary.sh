92c6ea5a98ca4e737a47b8e15a8b319dbf8cc7f1
Tarik <tarik.badawy@studium.uni-hamburg.de>
2024-01-19 20:51:36 +0100

    feat: add option to summary script

--- summary.sh
+++ summary.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-cat download.log | grep '^....-..-..' | cut -d' ' -f1 | sort | uniq -c
+if [ -z "$1" ] && [ ! -d "$1" ]; then
+    echo "Usage: $0 <data_dir>"
+    exit 1
+fi
+
+ls $1 | cut -d- -f1-3 | sort | uniq -c
